5f07338143efaff6e8d5a47f6f9590e5867e2e9c
marco-santulli <82715690+marco-santulli@users.noreply.github.com>
2025-01-09 15:57:35 +0100

    fix

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,5 +1,5 @@
 
-from pydantic import BaseModel, EmailStr, Field, root_validator
+from pydantic import BaseModel, EmailStr, Field, model_validator
 
 class UserBase(BaseModel):
     email: EmailStr
@@ -9,15 +9,13 @@ class UserCreate(UserBase):
     password: str = Field(None, min_length=8)
     hashed_password: str = None
 
-    @root_validator
-    def check_password_fields(cls, values):
-        password = values.get("password")
-        hashed_password = values.get("hashed_password")
-        if not password and not hashed_password:
+    @model_validator(mode="after")
+    def check_password_fields(self):
+        if not self.password and not self.hashed_password:
             raise ValueError("Either 'password' or 'hashed_password' must be provided.")
-        if password and hashed_password:
+        if self.password and self.hashed_password:
             raise ValueError("Only one of 'password' or 'hashed_password' can be provided.")
-        return values
+        return self
 
 class UserUpdate(BaseModel):
     email: EmailStr = None
