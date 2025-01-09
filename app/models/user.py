8783e36f70b79e8924a124836efbff74ff0afb14
marco-santulli <82715690+marco-santulli@users.noreply.github.com>
2025-01-09 16:19:15 +0100

    .

--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,9 @@ from sqlalchemy import Column, Integer, String
 from app.models.database import Base
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "t_user"
+
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+    full_name = Column(String, nullable=True)
